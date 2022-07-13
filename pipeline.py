38b907cbb26ff49b3432c8002c835512bc709abf
TLP <104315397+TLongP@users.noreply.github.com>
2022-07-13 20:11:19 +0200

    create a pipeline for loading data

---
+++ pipeline.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+def processing_text(line):
+    """
+    preprocessing each line 
+    remove special character . ? , !
+    return is a 2d array  
+    """
+    line = tf.strings.strip(line)
+    line = tf.strings.regex_replace(line,"[\.\?\!,]", "")
+    line = tf.strings.split(line,"\t")
+    return line
+
+
+
+
+def create_dataset(
+                    path,
+                    BUFFER_SIZE=10000,
+                    BATCH_SIZE=64,
+                    SEED=42
+                    ):
+
+    dataset = tf.data.TextLineDataset(path,buffer_size=BUFFER_SIZE)
+    dataset = dataset.map(lambda x: processing_text(x))
+    dataset = dataset.map(lambda x: {"question":x[0],"answer":x[1]})
+    return dataset.cache(
+                    ).shuffle(BUFFER_SIZE,seed=SEED
+                    ).batch(BATCH_SIZE
+                    ).prefetch(1)
+     
