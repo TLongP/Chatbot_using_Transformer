04a624f9155fca8e94bcfae03791d8401b3be5a1
TLP <104315397+TLongP@users.noreply.github.com>
2022-08-02 17:43:11 +0200

    add embedding loader

--- models/custom_blocks/__init__.py
+++ models/custom_blocks/__init__.py
@@ -1,1 +1,2 @@
 from .layers import EncoderLayer, DecoderLayer
+from .embedding import load_embedding_array
---
+++ models/custom_blocks/embedding.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+
+
+def load_embedding_array(path,vocab,encoding="utf-8"):
+    num_tokens = len(vocab)
+    word_index = dict(zip(vocab, range(len(vocab))))
+
+    embeddings_index = {}
+    with open(path,"r",encoding="utf-8") as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, "f", sep=" ")
+            embeddings_index[word] = coefs
+
+
+    for d in embeddings_index.values():
+        embedding_dim = len(d)
+        break
+
+    embedding_matrix = np.zeros((num_tokens, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            # This includes the representation for "padding" and "OOV"
+            embedding_matrix[i] = embedding_vector
+    return embedding_matrix
+  
